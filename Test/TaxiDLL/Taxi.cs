5d61ca0bdf4a9fe1fbe063143f5df78ca85b6ae9
midao <KonradAlwine@dv-schulen.de>
2017-03-09 08:42:51 +0100

    Taxi-Objekte hinzugefügt Auslesen der Taxis aus Datenbank

---
+++ Test/TaxiDLL/Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiDLL
+{
+    class Taxi
+    {
+
+        public int TaxiID { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+
+        public Taxi(int taxiID, double longitude, double latitude)
+        {
+            TaxiID = taxiID;
+            Longitude = longitude;
+            Latitude = latitude;
+
+        }
+
+
+    }
+}
